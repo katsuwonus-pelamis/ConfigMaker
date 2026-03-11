ff54143c26c575b4548fb27de02099a501542414
katsuwonus <numazorzi@gmail.com>
2026-03-11 16:44:07 +0100

    add cubic cell (to be debugged)

--- ConfigMaker/Cell.py
+++ ConfigMaker/Cell.py
@@ -6,8 +6,18 @@ class Cell:
     self.lattice_vectors = np.array(lattice_vectors)
     self.particles = particles
     self.npart = len(particles)
-    
-  def fcc_cell(a, l): #This is clearly not a minimal cell, but it's what we need to tile with orthogonal vectors and with the rods aligned in the (0 0 1) direction
+  
+  def cubic_cell(a, l, diam=1):
+    ori = [0,0,l]
+    cubic_cell = Cell(
+    lattice_vectors = [[a, 0, 0],                        
+                        [0, a, 0],                
+                        [0,0, l+a]],
+    particles=[ Spherocylinder([0, 0, 0], diam, 1, 'a')]
+    )
+    return cubic_cell
+  
+  def fcc_cell(a, l, diam=1): #This is clearly not a minimal cell, but it's what we need to tile with orthogonal vectors and with the rods aligned in the (0 0 1) direction
     ori = [0,0,l]
     h_step = l + np.sqrt(2/3) * a
 
@@ -16,18 +26,18 @@ class Cell:
                         [0, np.sqrt(3) * a, 0],                
                         [0, 0, 3*h_step]],
       particles=[
-                  Spherocylinder([0, 0, 0], ori, 1, 'a'),
-                  Spherocylinder([a, 0, 0], ori,1, 'a'),
-                  Spherocylinder([0.5*a, np.sqrt(3)/2*a, 0], ori,a, 'a'),
-                  Spherocylinder([1.5*a, np.sqrt(3)/2*a, 0], ori, a, 'a'),
-                  Spherocylinder([0.5, np.sqrt(3)/6, h_step], ori, a ,'a'),
-                  Spherocylinder([0, 2*np.sqrt(3)/3, h_step], ori, a ,'a'),
-                  Spherocylinder([1.5, np.sqrt(3)/6, h_step], ori, a ,'a'),
-                  Spherocylinder([1, 2*np.sqrt(3)/3, h_step], ori, a ,'a'),
-                  Spherocylinder([0,         (np.sqrt(3)/3)*a,               2*h_step], ori, 1, 'a'),
-                  Spherocylinder([1*a,       (np.sqrt(3)/3)*a,               2*h_step], ori, 1, 'a'),
-                  Spherocylinder([0.5*a,     (np.sqrt(3)/2 + np.sqrt(3)/3)*a,2*h_step], ori, 1, 'a'),
-                  Spherocylinder([1.5*a,     (np.sqrt(3)/2 + np.sqrt(3)/3)*a,2*h_step], ori, 1, 'a')]
+                  Spherocylinder([0, 0, 0], diam, 1, 'a'),
+                  Spherocylinder([a, 0, 0], diam,1, 'a'),
+                  Spherocylinder([0.5*a, np.sqrt(3)/2*a, 0], diam,a, 'a'),
+                  Spherocylinder([1.5*a, np.sqrt(3)/2*a, 0], diam, a, 'a'),
+                  Spherocylinder([0.5, np.sqrt(3)/6, h_step], diam, a ,'a'),
+                  Spherocylinder([0, 2*np.sqrt(3)/3, h_step], diam, a ,'a'),
+                  Spherocylinder([1.5, np.sqrt(3)/6, h_step], diam, a ,'a'),
+                  Spherocylinder([1, 2*np.sqrt(3)/3, h_step], diam, a ,'a'),
+                  Spherocylinder([0,         (np.sqrt(3)/3)*a,               2*h_step], ori, diam, 'a'),
+                  Spherocylinder([1*a,       (np.sqrt(3)/3)*a,               2*h_step], ori, diam, 'a'),
+                  Spherocylinder([0.5*a,     (np.sqrt(3)/2 + np.sqrt(3)/3)*a,2*h_step], ori, diam, 'a'),
+                  Spherocylinder([1.5*a,     (np.sqrt(3)/2 + np.sqrt(3)/3)*a,2*h_step], ori, diam, 'a')]
       )
     return fcc_cell
     
